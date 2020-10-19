3734eeb9e3aa419f4e70b96785fba863d01dff2e
Sim Reaney <sim.reaney@dur.ac.uk>
2020-10-19 10:12:28 +0100

    Appleby Flood Tour

---
+++ CMM/appleby-flood-tour/app-files/data.js
@@ -0,0 +1,595 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sands-area-right-bank-of-eden",
+      "name": "Sands area, right bank of Eden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.835859028799238,
+          "pitch": -0.01610288149469241,
+          "rotation": 0,
+          "target": "1-property-level-protection-sands"
+        },
+        {
+          "yaw": -1.5513156951504854,
+          "pitch": -0.0006348434827927463,
+          "rotation": 0,
+          "target": "7-bridge-over-the-river-eden"
+        },
+        {
+          "yaw": -1.7562449713824346,
+          "pitch": 0.048509988591893816,
+          "rotation": 0,
+          "target": "5-sands-area-right-bank-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-property-level-protection-sands",
+      "name": "Property level protection, Sands",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1133252734155423,
+          "pitch": 0.0035264116712383498,
+          "rotation": 0,
+          "target": "5-sands-area-right-bank-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-raised-infrastructure",
+      "name": "Raised infrastructure",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-flood-water-mark",
+      "name": "Flood water mark",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04324769279165608,
+          "pitch": 0.08809387884346798,
+          "rotation": 0,
+          "target": "14-flood-gates-edge-of-playing-field"
+        },
+        {
+          "yaw": 0.5968622875852212,
+          "pitch": 0.05168139362304203,
+          "rotation": 0,
+          "target": "10-swimming-pool-car-park"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-appleby-high-street",
+      "name": "Appleby High Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sands-area-right-bank-2",
+      "name": "Sands area, right bank 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5401055653806246,
+          "pitch": -0.0830244084827445,
+          "rotation": 0,
+          "target": "7-bridge-over-the-river-eden"
+        },
+        {
+          "yaw": 0.8723943907463774,
+          "pitch": 0.06520372274929542,
+          "rotation": 0,
+          "target": "0-sands-area-right-bank-of-eden"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lower-part-of-appleby-high-street",
+      "name": "Lower part of Appleby High Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3560529063665072,
+          "pitch": -0.014629836473455171,
+          "rotation": 0,
+          "target": "3-flood-water-mark"
+        },
+        {
+          "yaw": 3.0010036086540897,
+          "pitch": -0.01596510665715556,
+          "rotation": 0,
+          "target": "4-appleby-high-street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bridge-over-the-river-eden",
+      "name": "Bridge over the River Eden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.565979857634284,
+          "pitch": -0.021009196740529035,
+          "rotation": 0,
+          "target": "2-raised-infrastructure"
+        },
+        {
+          "yaw": -1.6574225721020746,
+          "pitch": 0.04394426467509405,
+          "rotation": 0,
+          "target": "6-lower-part-of-appleby-high-street"
+        },
+        {
+          "yaw": 0.6910340558758801,
+          "pitch": 0.22004712827897066,
+          "rotation": 0,
+          "target": "5-sands-area-right-bank-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-river-eden-near-swimming-baths",
+      "name": "River Eden near Swimming baths",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.15436272074796,
+          "pitch": -0.07891261313801934,
+          "rotation": 0,
+          "target": "9-cricket-pitch-and-river-eden-left-bank"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-cricket-pitch-and-river-eden-left-bank",
+      "name": "Cricket Pitch and River Eden Left bank",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-swimming-pool-car-park",
+      "name": "Swimming pool car park",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4957852139009997,
+          "pitch": -0.035029980406463324,
+          "rotation": 0,
+          "target": "8-river-eden-near-swimming-baths"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-doomsgate",
+      "name": "Doomsgate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-bridge-over-eden-downstream",
+      "name": "Bridge over Eden downstream",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1283782738963826,
+          "pitch": 0.0644106290018982,
+          "rotation": 0,
+          "target": "11-doomsgate"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-flood-gates-by-toilets",
+      "name": "Flood Gates by toilets",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9285513280515403,
+          "pitch": -0.029230885112802696,
+          "rotation": 0,
+          "target": "12-bridge-over-eden-downstream"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-flood-gates-edge-of-playing-field",
+      "name": "Flood Gates edge of playing field",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37341752047407617,
+          "pitch": 0.0030707034937282884,
+          "rotation": 0,
+          "target": "13-flood-gates-by-toilets"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Appleby Flood Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
