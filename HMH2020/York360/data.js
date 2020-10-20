59b8aad1f8d999ceea48f10f77fc2d829fa4b3b2
Sim Reaney <sim.reaney@dur.ac.uk>
2020-10-20 17:33:02 +0100

    York360

---
+++ HMH2020/York360/data.js
@@ -0,0 +1,439 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-flood-gates-and-walls-near-marygate",
+      "name": "Flood Gates and walls near Marygate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6173359818788224,
+          "pitch": -0.028758495936973105,
+          "rotation": 0,
+          "target": "1-engineered-banks-on-jundi-dench-walk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineered-banks-on-jundi-dench-walk",
+      "name": "Engineered banks on Jundi Dench Walk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.340279369711375,
+          "pitch": 0.13107458699118624,
+          "rotation": 0,
+          "target": "0-flood-gates-and-walls-near-marygate"
+        },
+        {
+          "yaw": -1.6782779408658115,
+          "pitch": -0.004290539523235992,
+          "rotation": 0,
+          "target": "2-building-design-and-gates-wellington-row"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-building-design-and-gates-wellington-row",
+      "name": "Building design and gates Wellington Row",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5606838613350185,
+          "pitch": -0.051432970120458776,
+          "rotation": 0,
+          "target": "1-engineered-banks-on-jundi-dench-walk"
+        },
+        {
+          "yaw": -0.44456832339875874,
+          "pitch": -0.06760212098420659,
+          "rotation": 0,
+          "target": "3-behind-flood-wall-wellington-row"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-behind-flood-wall-wellington-row",
+      "name": "Behind flood wall Wellington Row",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6345574043720745,
+          "pitch": -0.0388098404483479,
+          "rotation": 0,
+          "target": "2-building-design-and-gates-wellington-row"
+        },
+        {
+          "yaw": 1.8830044258634988,
+          "pitch": -0.11227798472160799,
+          "rotation": 0,
+          "target": "4-flood-proofed-urban-landscape-terry-av"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-flood-proofed-urban-landscape-terry-av",
+      "name": "Flood proofed urban landscape Terry Av",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2353622931727841,
+          "pitch": -0.06349943002604164,
+          "rotation": 0,
+          "target": "3-behind-flood-wall-wellington-row"
+        },
+        {
+          "yaw": 1.029060863027759,
+          "pitch": 0.04442950934744161,
+          "rotation": 0,
+          "target": "5-foss-barrier-ds"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-foss-barrier-ds",
+      "name": "Foss Barrier DS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5430856496282477,
+          "pitch": -0.07463566286211254,
+          "rotation": 0,
+          "target": "4-flood-proofed-urban-landscape-terry-av"
+        },
+        {
+          "yaw": 0.5741631994414078,
+          "pitch": 0.06647022318286133,
+          "rotation": 0,
+          "target": "6-foss-barrier-us"
+        },
+        {
+          "yaw": -1.9508820779618752,
+          "pitch": -0.04534346845284709,
+          "rotation": 0,
+          "target": "7-flood-proof-in-arch-clementhorpe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-foss-barrier-us",
+      "name": "Foss Barrier US",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0028013003487448174,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-foss-barrier-ds"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-flood-proof-in-arch-clementhorpe",
+      "name": "Flood Proof in Arch Clementhorpe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9983788068583017,
+          "pitch": 0.019889988341677878,
+          "rotation": 0,
+          "target": "5-foss-barrier-ds"
+        },
+        {
+          "yaw": 2.6817853544709482,
+          "pitch": 0.13210566251588496,
+          "rotation": 0,
+          "target": "8-flood-proofing-in-arch-river-street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-flood-proofing-in-arch-river-street",
+      "name": "Flood proofing in arch River Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.487030698425949,
+          "pitch": 0.13753208517002413,
+          "rotation": 0,
+          "target": "9-river-street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-river-street",
+      "name": "River Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9039694051440996,
+          "pitch": -0.06977678812243227,
+          "rotation": 0,
+          "target": "8-flood-proofing-in-arch-river-street"
+        },
+        {
+          "yaw": 0.5659549373702859,
+          "pitch": 0.11872700269954706,
+          "rotation": 0,
+          "target": "7-flood-proof-in-arch-clementhorpe"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "York Flood Defences",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
