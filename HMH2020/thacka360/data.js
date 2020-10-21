fd8f4506689f4afeaeeff2a9c4fa8a9cc3bfc09e
Sim Reaney <sim.reaney@dur.ac.uk>
2020-10-21 16:10:09 +0100

    thacka 360

---
+++ HMH2020/thacka360/data.js
@@ -0,0 +1,557 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01-inflow",
+      "name": "01 Inflow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4069646369447426,
+          "pitch": 0.2566583273254608,
+          "rotation": 0,
+          "target": "1-02-project-sign"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02-project-sign",
+      "name": "02 Project sign",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0533611109351462,
+          "pitch": 0.02878992136632519,
+          "rotation": 0,
+          "target": "2-03-right-side-1"
+        },
+        {
+          "yaw": -1.2609454053972335,
+          "pitch": 0.07811091545638149,
+          "rotation": 0,
+          "target": "0-01-inflow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03-right-side-1",
+      "name": "03 Right side 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6588567830720216,
+          "pitch": -0.006269320181116811,
+          "rotation": 0,
+          "target": "3-04-right-side-2"
+        },
+        {
+          "yaw": -1.9991150839568732,
+          "pitch": 0.052430085733112364,
+          "rotation": 0,
+          "target": "1-02-project-sign"
+        },
+        {
+          "yaw": 0.0289142563314968,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "8-09-on-barrier-on-top"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04-right-side-2",
+      "name": "04 Right side 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8146674160498915,
+          "pitch": 0.29465804851246524,
+          "rotation": 0,
+          "target": "4-05-right-side-3"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-03-right-side-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05-right-side-3",
+      "name": "05 Right side 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.017293882589577,
+          "pitch": 0.07677438474205722,
+          "rotation": 0,
+          "target": "3-04-right-side-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06-right-side-4",
+      "name": "06 Right side 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9930943482071051,
+          "pitch": 0.012538640362235398,
+          "rotation": 0,
+          "target": "6-06-right-side-5"
+        },
+        {
+          "yaw": -1.3184797395837364,
+          "pitch": -0.07127894954495417,
+          "rotation": 0,
+          "target": "4-05-right-side-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-06-right-side-5",
+      "name": "06 Right side 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3153089793445183,
+          "pitch": 0.04701990135837164,
+          "rotation": 0,
+          "target": "7-07-by-interpretation-board"
+        },
+        {
+          "yaw": -1.1133236881837547,
+          "pitch": -0.034481260996139795,
+          "rotation": 0,
+          "target": "5-06-right-side-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-07-by-interpretation-board",
+      "name": "07 By interpretation board",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7358075882470949,
+          "pitch": 0.1473290242562335,
+          "rotation": 0,
+          "target": "8-09-on-barrier-on-top"
+        },
+        {
+          "yaw": 0.42003887034632825,
+          "pitch": 0.09499792908964011,
+          "rotation": 0,
+          "target": "9-10-on-barrier-by-inlet"
+        },
+        {
+          "yaw": 0.9536991219843021,
+          "pitch": 0.028423767005829603,
+          "rotation": 0,
+          "target": "10-11-on-barrier-by-outlet"
+        },
+        {
+          "yaw": -1.008098054797788,
+          "pitch": 0.012538640362231845,
+          "rotation": 0,
+          "target": "6-06-right-side-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09-on-barrier-on-top",
+      "name": "09 On barrier, on top",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.935374129427162,
+          "pitch": 0.3802720038950653,
+          "rotation": 0,
+          "target": "9-10-on-barrier-by-inlet"
+        },
+        {
+          "yaw": -2.069631954236648,
+          "pitch": 0.08455963190364102,
+          "rotation": 0,
+          "target": "10-11-on-barrier-by-outlet"
+        },
+        {
+          "yaw": -0.44884031817833225,
+          "pitch": 0.187361143297041,
+          "rotation": 0,
+          "target": "7-07-by-interpretation-board"
+        },
+        {
+          "yaw": 1.6750026346497293,
+          "pitch": -0.002445433580184897,
+          "rotation": 0,
+          "target": "0-01-inflow"
+        },
+        {
+          "yaw": -3.0683804578191687,
+          "pitch": 0.0022567179512158475,
+          "rotation": 0,
+          "target": "11-12-on-barrier-passed-centre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10-on-barrier-by-inlet",
+      "name": "10 On barrier, by inlet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8179522055825057,
+          "pitch": -0.05138377221405577,
+          "rotation": 0,
+          "target": "10-11-on-barrier-by-outlet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11-on-barrier-by-outlet",
+      "name": "11 On Barrier, by outlet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1902704657688101,
+          "pitch": 0.20375290588628125,
+          "rotation": 0,
+          "target": "11-12-on-barrier-passed-centre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12-on-barrier-passed-centre",
+      "name": "12 On barrier, passed centre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1562668171337043,
+          "pitch": 0.07792155024587011,
+          "rotation": 0,
+          "target": "10-11-on-barrier-by-outlet"
+        },
+        {
+          "yaw": 2.6807809398797042,
+          "pitch": 0.129713399902478,
+          "rotation": 0,
+          "target": "8-09-on-barrier-on-top"
+        },
+        {
+          "yaw": 3.0871561090007704,
+          "pitch": 0.16419466089861778,
+          "rotation": 0,
+          "target": "9-10-on-barrier-by-inlet"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Thacka Beck FAS, Penrith, Cumbria",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
